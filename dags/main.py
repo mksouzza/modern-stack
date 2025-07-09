289206391c83a0d300b4207d19b682d26b476f83
mksouzza <mksouzza@gmail.com>
2025-07-09 00:41:23 -0300

    fix dag

--- dags/main.py
+++ dags/main.py
@@ -1,31 +1,30 @@
 from time import sleep
 
-from airflow.models.dag import DAG
-from airflow.decorators import task
+from airflow.sdk import DAG, task
 from datetime import datetime
 
 with DAG(dag_id = "test", description = "new etl", schedule = "@daily", start_date=datetime(2025,7,9), catchup=False) as dag:
-def pipeline():
+    def pipeline():
 
-    @task
-    def first_activity():
-        print("first activity ran successfully")
-        sleep(2)
+        @task
+        def first_activity():
+            print("first activity ran successfully")
+            sleep(2)
 
-    @task
-    def second_activity():
-        print("second activity ran successfully")
-        sleep(2)
+        @task
+        def second_activity():
+            print("second activity ran successfully")
+            sleep(2)
 
-    @task
-    def third_activity():
-        print("third activity ran successfully")
-        sleep(2)
+        @task
+        def third_activity():
+            print("third activity ran successfully")
+            sleep(2)
 
-    t1 = first_activity()
-    t2 = second_activity()
-    t3 = third_activity()
+        t1 = first_activity()
+        t2 = second_activity()
+        t3 = third_activity()
 
-    t1 >> t2 >> t3
+        t1 >> t2 >> t3
 
 pipeline()
